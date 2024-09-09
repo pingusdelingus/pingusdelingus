621bd69757d5e401ccddd8a9059cd2712ace3efb
pingusdelingus <alphaspammail2016@gmail.com>
2024-09-08 23:18:06 -0400

    fixed the error with extra '

--- console.sh
+++ console.sh
@@ -3,10 +3,10 @@
 echo "Last login: $(date +'%a, %d %b %Y at %H:%M:%S %Z') from 10.1.33.7"
 echo
 echo '$ echo "hello world, im $(whoami)."'
-echo 'hello world, i'm esteban.'
+echo 'hello world, im esteban.'
 echo
 echo '$ ls ~/interests'
-echo `ls interests`
+echo $(ls interests)
 echo
 echo '$ head -1 /etc/hosts'
 echo '127.0.0.1 localhost miami, fl '
