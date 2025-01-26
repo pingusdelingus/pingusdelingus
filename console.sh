a3c3225c05c3d64e76febce3d4f01dedd51fba2c
pingusdelingus <157940787+pingusdelingus@users.noreply.github.com>
2025-01-25 21:50:03 -0500

    Update console.sh

--- console.sh
+++ console.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+echo " ```bash " 
 echo "Last login: $(date +'%a, %d %b %Y at %H:%M:%S %Z') from 10.1.33.7"
 echo
 echo "hello world, im $(whoami)."
@@ -12,3 +13,4 @@ echo "$ head -1 /etc/hosts"
 echo '127.0.0.1 localhost miami, fl '
 echo
 echo "$ # do one thing and do it well. ^U^D"
+echo " ``` "
