7600d4ecd68ec1d359548bf2df6284b70b3df65e
pingusdelingus <157940787+pingusdelingus@users.noreply.github.com>
2025-01-28 17:23:54 -0500

    Update console.sh fixe!

--- console.sh
+++ console.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-echo " ```bash " 
 echo "Last login: $(date +'%a, %d %b %Y at %H:%M:%S %Z') from 10.1.33.7"
 echo
-echo "hello world, im $(whoami)."
+echo 'echo "hello world, im $(whoami)." '
 echo 'hello world, im esteban.'
 echo
 echo "$ ls ~/interests"
-echo $(ls interests)
+ls interests
 echo
 echo "$ head -1 /etc/hosts"
-echo '127.0.0.1 localhost miami, fl '
+echo "127.0.0.1 localhost miami, fl "
 echo
-echo "$ # do one thing and do it well. ^U^D"
-echo " ``` "
+echo "$ do one thing and do it well. ^U^D "
+echo
